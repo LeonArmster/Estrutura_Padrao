addd145d21202a49f99b5268f43cf97d080926f3
Leonardo Souza <leo.armster1994@gmail.com>
2025-05-10 21:43:08 -0300

    Transform funcionando e validado pela pytest

---
+++ app/pipeline/extract.py
@@ -0,0 +1,32 @@
+# Bibliotecas
+import os # biblioteca para manipular arquivos e pastas
+import glob # biblioteca para listar arquivos
+import pandas as pd # biblioteca para trabalhar arquivos
+
+"""
+Função para ler os arquivos de uma pasta e retornar uma lista de dataframes
+
+args: input_path (str): caminho da pasta com os arquivos
+
+return: lista de dataframes
+"""
+
+
+
+
+def extract_from_excel(path:str) -> pd.DataFrame:
+    lista_arquivo = glob.glob(os.path.join(path, '*x.xlsx'))
+    df_list = []
+
+    for arquivo in lista_arquivo:
+        df_list.append(pd.read_excel(arquivo, engine='openpyxl'))
+
+    #df = pd.concat(df_list)
+
+    return df_list
+
+if __name__ == "__main__":
+    path = r'C:\Users\Uso Pessoal\desktop\estudos\jornada_dados\estrutura_projeto\data'
+    df = extract_from_excel(path=path)
+    print('hello word')
+    print(df)
---
+++ app/pipeline/transform.py
@@ -0,0 +1,10 @@
+# Bibliotecas
+import pandas as pd
+from typing import List
+
+"""
+Função para transformar uma lista de dataframes em um único dataframe
+"""
+
+def concat_dataframe(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    return pd.concat(data_frame_list, ignore_index=True)
